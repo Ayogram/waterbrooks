9b452032b11b41361255c5c4bab493aa5166cabf
Ayo <ajumobiayomipo@gmail.com>
2026-03-20 11:50:35 +0100

    Swapped bcrypt for universal bcryptjs to fix Vercel crash completely

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
